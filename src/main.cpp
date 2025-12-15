77a0790c08d6e3398a88e12b113e693f0cf1fa78
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-12-15 21:54:04 +0000

    working demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,67 @@
-#include <Arduino.h>
+#include <Arduino_GFX_Library.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define EXAMPLE_PIN_NUM_LCD_SCLK 39
+#define EXAMPLE_PIN_NUM_LCD_MOSI 38
+#define EXAMPLE_PIN_NUM_LCD_MISO 40
+#define EXAMPLE_PIN_NUM_LCD_DC 42
+#define EXAMPLE_PIN_NUM_LCD_RST -1
+#define EXAMPLE_PIN_NUM_LCD_CS 45
+#define EXAMPLE_PIN_NUM_LCD_BL 1
 
-void setup()
+#define EXAMPLE_LCD_ROTATION 1
+#define EXAMPLE_LCD_H_RES 240
+#define EXAMPLE_LCD_V_RES 320
+
+/* More data bus class: https://github.com/moononournation/Arduino_GFX/wiki/Data-Bus-Class */
+Arduino_DataBus *bus = new Arduino_ESP32SPI(
+    EXAMPLE_PIN_NUM_LCD_DC /* DC */, EXAMPLE_PIN_NUM_LCD_CS /* CS */,
+    EXAMPLE_PIN_NUM_LCD_SCLK /* SCK */, EXAMPLE_PIN_NUM_LCD_MOSI /* MOSI */, EXAMPLE_PIN_NUM_LCD_MISO /* MISO */);
+
+/* More display class: https://github.com/moononournation/Arduino_GFX/wiki/Display-Class */
+Arduino_GFX *gfx = new Arduino_ST7789(
+    bus, EXAMPLE_PIN_NUM_LCD_RST /* RST */, EXAMPLE_LCD_ROTATION /* rotation */, true /* IPS */,
+    EXAMPLE_LCD_H_RES /* width */, EXAMPLE_LCD_V_RES /* height */);
+
+/*******************************************************************************
+ * End of Arduino_GFX setting
+ ******************************************************************************/
+
+void setup(void)
 {
   Serial.begin(115200);
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Serial.setDebugOutput(true);
+  // while(!Serial);
+  Serial.println("Arduino_GFX Hello World example");
+
+#ifdef GFX_EXTRA_PRE_INIT
+  GFX_EXTRA_PRE_INIT();
+#endif
+
+  // Init Display
+  if (!gfx->begin())
+  {
+    Serial.println("gfx->begin() failed!");
+  }
+  gfx->fillScreen(RGB565_BLACK);
+
+#ifdef EXAMPLE_PIN_NUM_LCD_BL
+  pinMode(EXAMPLE_PIN_NUM_LCD_BL, OUTPUT);
+  digitalWrite(EXAMPLE_PIN_NUM_LCD_BL, HIGH);
+#endif
+
+  gfx->setCursor(10, 10);
+  gfx->setTextColor(RGB565_RED);
+  gfx->println("Hello World!");
+
+  delay(5000); // 5 seconds
 }
 
 void loop()
 {
-  Serial.println("Hello, World!");
-  delay(1000);
-  // put your main code here, to run repeatedly:
-}
+  gfx->setCursor(random(gfx->width()), random(gfx->height()));
+  gfx->setTextColor(random(0xffff), random(0xffff));
+  gfx->setTextSize(random(6) /* x scale */, random(6) /* y scale */, random(2) /* pixel_margin */);
+  gfx->println("Hello World!");
 
-// put function definitions here:
-int myFunction(int x, int y)
-{
-  return x + y;
+  delay(1000); // 1 second
 }
