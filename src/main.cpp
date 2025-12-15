3d8818419d2c6e4079cdbb48378ec436279ee700
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-12-15 21:19:25 +0000

    init project

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup()
+{
+  Serial.begin(115200);
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop()
+{
+  Serial.println("Hello, World!");
+  delay(1000);
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y)
+{
+  return x + y;
+}
